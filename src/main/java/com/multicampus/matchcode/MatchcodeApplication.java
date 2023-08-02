e17ad8a3f83e94325df0ebc70120a88acae6dd18
Keitiankor <keitian.kor@gmail.com>
2023-08-02 09:38:43 +0900

    [Initial] Initial commit

---
+++ src/main/java/com/multicampus/matchcode/MatchcodeApplication.java
@@ -0,0 +1,14 @@
+package com.multicampus.matchcode;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class MatchcodeApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(MatchcodeApplication.class)
+            .properties("spring.config.location=classpath:application.yml")
+            .run(args);
+    }
+}
---
+++ src/test/java/com/multicampus/matchcode/MatchcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multicampus.matchcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
