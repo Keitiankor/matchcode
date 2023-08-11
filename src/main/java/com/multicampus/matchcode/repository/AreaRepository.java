e92f414b5537e4f192faa0bdeb96957b11a22e41
Keitiankor <keitian.kor@gmail.com>
2023-08-11 13:39:59 +0900

    [Add] AreaRepository

---
+++ src/main/java/com/multicampus/matchcode/repository/AreaRepository.java
@@ -0,0 +1,6 @@
+package com.multicampus.matchcode.repository;
+
+import com.multicampus.matchcode.model.entity.AreaDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AreaRepository extends JpaRepository<AreaDTO, Long> {}
