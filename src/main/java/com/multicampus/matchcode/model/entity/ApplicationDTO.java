766d4d3964d4ea9b7314b1ba35ffb9d0b7bddc19
Keitiankor <keitian.kor@gmail.com>
2023-08-18 11:30:01 +0900

    [Fix] 엔티티네임 문제있던것 수정

--- src/main/java/com/multicampus/matchcode/model/entity/ApplicationDTO.java
+++ src/main/java/com/multicampus/matchcode/model/entity/ApplicationDTO.java
@@ -7,7 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "Member")
+@Entity(name = "Application")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/multicampus/matchcode/model/entity/MatchDTO.java
+++ src/main/java/com/multicampus/matchcode/model/entity/MatchDTO.java
@@ -8,7 +8,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "Member")
+@Entity(name = "Match")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
