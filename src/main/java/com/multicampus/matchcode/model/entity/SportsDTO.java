02e2fb021f155c7ea1cb9e4ceb1971b077f49eae
Keitiankor <keitian.kor@gmail.com>
2023-08-18 10:45:10 +0900

    [Fix] DTO의 어노테이션 추가

--- src/main/java/com/multicampus/matchcode/model/entity/SportsDTO.java
+++ src/main/java/com/multicampus/matchcode/model/entity/SportsDTO.java
@@ -2,10 +2,16 @@ package com.multicampus.matchcode.model.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Entity(name = "Sports")
+@Entity(name = "Member")
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SportsDTO {
 
     @Id
