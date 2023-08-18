cd0c8d46690df3e4eed6af1ab45719cd53f039a5
hyem808 <shmty510@gmail.com>
2023-08-18 13:36:04 +0900

    [Init] 초기 파일 작업 기초 뼈대를 잡기 위해 패키지와 Controller, Service 파일을 생성함.

---
+++ src/main/java/com/multicampus/matchcode/controller/hyem/TeamController.java
@@ -0,0 +1,9 @@
+package com.multicampus.matchcode.controller.hyem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/team")
+public class TeamController {
+}
---
+++ src/main/java/com/multicampus/matchcode/service/hyem/TeamService.java
@@ -0,0 +1,7 @@
+package com.multicampus.matchcode.service.hyem;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TeamService {
+}
