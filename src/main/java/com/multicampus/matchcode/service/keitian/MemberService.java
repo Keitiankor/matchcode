4a21ab76ef553a2a50e7e20730bbc65c0ff978da
Keitiankor <keitian.kor@gmail.com>
2023-08-18 15:14:03 +0900

    [Add] 세션 상수

---
+++ src/main/java/com/multicampus/matchcode/service/keitian/MemberService.java
@@ -0,0 +1,47 @@
+package com.multicampus.matchcode.service.keitian;
+
+import com.multicampus.matchcode.model.entity.MemberDTO;
+import com.multicampus.matchcode.model.request.keitian.LoginRequest;
+import com.multicampus.matchcode.model.request.keitian.RegistserRequest;
+import com.multicampus.matchcode.repository.MemberRepository;
+import jakarta.servlet.http.HttpSession;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    MemberRepository repository;
+
+    public void insert(RegistserRequest request) {
+        MemberDTO dto = MemberDTO
+            .builder()
+            .areaId(request.getAreaId())
+            .account(request.getAccount())
+            .password(request.getPassword())
+            .name(request.getName())
+            .phone(request.getPhone())
+            .mailAddress(request.getMailAddress())
+            .birthday(request.getBirthday())
+            .build();
+
+        repository.save(dto);
+    }
+
+    public void login(LoginRequest request) {
+        long id = getId(request.getAccount(), request.getPassword());
+        if (id != -1) {}
+    }
+
+    private long getId(String acc, String pw) {
+        Optional<MemberDTO> odto = repository.findByAccountAndPassword(acc, pw);
+        if (odto.isPresent()) {
+            MemberDTO dto = odto.get();
+            return dto.getId();
+        } else {
+            return -1;
+        }
+    }
+}
