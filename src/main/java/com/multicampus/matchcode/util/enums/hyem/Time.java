bebeef0e6913dabee4ffcef9f920f514504f2994
hyem808 <shmty510@gmail.com>
2023-08-30 11:44:03 +0900

    [Refactor] enum 수정 name을 영어로 수정하고, description에 한글 설명을 수정하였음.

--- src/main/java/com/multicampus/matchcode/util/enums/hyem/Time.java
+++ src/main/java/com/multicampus/matchcode/util/enums/hyem/Time.java
@@ -4,21 +4,23 @@ import lombok.Getter;
 
 @Getter
 public enum Time {
-    아침(1),
-    낮(2),
-    저녁(3),
-    심야(4);
+    MORNING("아침", 1),
+    AFTERNOON("낮", 2),
+    EVENING("저녁", 3),
+    NIGHT("심야", 4);
 
+    private final String description;
     private final Integer count;
 
-    Time(Integer count) {
+    Time(String description, Integer count) {
+        this.description = description;
         this.count = count;
     }
 
-    public static String getNameFromCount(Integer count) {
+    public static String getDescriptionFromCount(Integer count) {
         for (Time time : Time.values()) {
             if (time.getCount().equals(count)) {
-                return time.name();
+                return time.getDescription();
             }
         }
         return "Unknown";
