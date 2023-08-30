3cb98bba6b2fab5dfe47d489f5e367e5c3adb76d
hyem808 <shmty510@gmail.com>
2023-08-30 12:39:32 +0900

    [Refactor] enum 수정 name을 모두 영어로 전환하고, description을 추가하여 한글로 표기. count에서 description을 찾을 수 있는 메서드로 수정.

--- src/main/java/com/multicampus/matchcode/util/enums/hyem/Sport.java
+++ src/main/java/com/multicampus/matchcode/util/enums/hyem/Sport.java
@@ -4,26 +4,24 @@ import lombok.Getter;
 
 @Getter
 public enum Sport {
-    농구(1),
-    풋살(2),
-    배드민턴(3);
+    BASKETBALL("농구", 1),
+    FUTSAL("풋살", 2),
+    BADMINTON("배드민턴", 3);
 
+    private final String description;
     private final Integer count;
 
-    Sport(Integer count) {
+    Sport(String description, Integer count) {
+        this.description = description;
         this.count = count;
     }
 
-    public static String getNameFromCount(Integer count) {
+    public static String getDescriptionFromCount(Integer count) {
         for (Sport sport : Sport.values()) {
             if (sport.getCount().equals(count)) {
-                return sport.name();
+                return sport.getDescription();
             }
         }
         return "Unknown";
-    }
-
-    public static int getSize() {
-        return Sport.values().length;
     }
 }
--- src/main/java/com/multicampus/matchcode/util/enums/hyem/Week.java
+++ src/main/java/com/multicampus/matchcode/util/enums/hyem/Week.java
@@ -4,24 +4,26 @@ import lombok.Getter;
 
 @Getter
 public enum Week {
-    월(1),
-    화(2),
-    수(3),
-    목(4),
-    금(5),
-    토(6),
-    일(7);
+    MON("월", 1),
+    TUE("화", 2),
+    WED("수", 3),
+    THU("목", 4),
+    FRI("금", 5),
+    SAT("토", 6),
+    SUN("일", 7);
 
+    private final String description;
     private final Integer count;
 
-    Week(Integer count) {
+    Week(String description, Integer count) {
+        this.description = description;
         this.count = count;
     }
 
-    public static String getNameFromCount(Integer count) {
+    public static String getDescriptionFromCount(Integer count) {
         for (Week week : Week.values()) {
             if (week.getCount().equals(count)) {
-                return week.name();
+                return week.getDescription();
             }
         }
         return "Unknown";
