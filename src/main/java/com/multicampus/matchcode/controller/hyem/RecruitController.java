ffb3073e7b85219e988a48bc7ff309418ede386d
hyem808 <shmty510@gmail.com>
2023-09-02 02:16:26 +0900

    [Add] 모집글 여부 확인

--- src/main/java/com/multicampus/matchcode/controller/hyem/RecruitController.java
+++ src/main/java/com/multicampus/matchcode/controller/hyem/RecruitController.java
@@ -30,16 +30,21 @@ public class RecruitController {
     // 모집글 작성 매핑
     @GetMapping("/write/{teamid}")
     public String writeRecruit(@PathVariable("teamid") Long teamId, Model model) {
-        RecruitDTO recruitDTO = new RecruitDTO();
-        model.addAttribute("recruit", recruitDTO);
-        return "hyem/recruit/writerecruit";
+        if (recruitService.isRecruitExist(teamId)) {
+            model.addAttribute("message", "이미 모집중입니다.");
+            model.addAttribute("searchUrl", "/team/list"); // 임시 mapping 주소
+            return "hyem/message";
+        } else {
+            RecruitDTO recruitDTO = new RecruitDTO();
+            model.addAttribute("recruit", recruitDTO);
+            return "hyem/recruit/writerecruit";
+        }
     }
 
     // 모집글 작성 처리
     @PostMapping("/write/{id}")
     public String addRecruit(
-            @ModelAttribute("recruit") RecruitPostRequest request, Long teamId, Model model
-    ) {
+            @ModelAttribute("recruit") RecruitPostRequest request, Long teamId, Model model) {
         model.addAttribute("teamid", teamId);
         recruitService.save(request);
         System.out.println("content : " + request.getContent());
@@ -50,15 +55,13 @@ public class RecruitController {
     @GetMapping("/list")
     public String recruitList(
             @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
-            Model model
-    ) {
+            Model model) {
         Page<RecruitListRequest> list = recruitService.getRecruitsWithTeamNames();
 
         model.addAttribute("list", list);
 
-        int nowPage = list
-                .getPageable()
-                .getPageNumber() + 1;
+        int nowPage = list.getPageable()
+                          .getPageNumber() + 1;
         int startPage = Math.max(nowPage - 4, 1);
         int endPage = Math.min(nowPage + 5, list.getTotalPages());
 
@@ -74,8 +77,7 @@ public class RecruitController {
     public String recruitView(
             @PathVariable("id") Long id,
             Model model,
-            @SessionAttribute(name = SessionConstant.MEMBER_DTO, required = false) MemberDTO memberDTO
-    ) {
+            @SessionAttribute(name = SessionConstant.MEMBER_DTO, required = false) MemberDTO memberDTO) {
 
         if (memberDTO != null) {
             RecruitDTO recruitDTO = recruitService.recruitView(id);
@@ -100,8 +102,9 @@ public class RecruitController {
     // 모집글 내용 수정
     @PostMapping("/modify/complete/{id}")
     public String recruitUpdate(
-            @RequestParam("id") Long id, @ModelAttribute("recruit") RecruitPostRequest request, Model model
-    ) throws Exception {
+            @RequestParam("id") Long id,
+            @ModelAttribute("recruit") RecruitPostRequest request,
+            Model model) throws Exception {
         recruitService.recruitUpdate(id, request);
         model.addAttribute("message", "모집글 수정이 완료되었습니다.");
         model.addAttribute("searchUrl", "/recruit/list");
