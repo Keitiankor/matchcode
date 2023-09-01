c55e70993a3da48b8e17cff0ec0608c177831bf8
hyem808 <shmty510@gmail.com>
2023-09-01 10:54:33 +0900

    [Refactor] 연관관계 수정에 따른 리팩토링 팀 삭제시 관련 모집글도 삭제되는 기능 추가됨

--- src/main/java/com/multicampus/matchcode/service/hyem/TeamService.java
+++ src/main/java/com/multicampus/matchcode/service/hyem/TeamService.java
@@ -1,19 +1,19 @@
 package com.multicampus.matchcode.service.hyem;
 
 import com.multicampus.matchcode.model.entity.TeamDTO;
+import com.multicampus.matchcode.model.entity.TeamMemberDTO;
 import com.multicampus.matchcode.model.request.hyem.TeamCreateRequest;
+import com.multicampus.matchcode.model.request.hyem.TeamMemberRequest;
+import com.multicampus.matchcode.repository.RecruitRepository;
+import com.multicampus.matchcode.repository.TeamMemberRepository;
 import com.multicampus.matchcode.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Service
 public class TeamService {
@@ -21,19 +21,35 @@ public class TeamService {
     @Autowired
     private TeamRepository teamRepository;
 
+    @Autowired
+    private RecruitRepository recruitRepository;
+
+    @Autowired
+    private TeamMemberRepository teamMemberRepository;
+
     // 팀 생성
-    public void save(TeamCreateRequest request) {
+    public void createTeam(TeamCreateRequest request) {
         TeamDTO dto = TeamDTO
-            .builder()
-            .sportsId(request.getSportsId())
-            .teamName(request.getTeamName())
-            .uri(request.getUri())
-            .useWeek(request.getUseWeek())
-            .useTime(request.getUseTime())
-            .averageAge(request.getAverageAge())
-            .averageGender(request.getAverageGender())
-            .build();
+                .builder()
+                .sportsId(request.getSportsId())
+                .teamName(request.getTeamName())
+                .uri(request.getUri())
+                .useWeek(request.getUseWeek())
+                .useTime(request.getUseTime())
+                .averageAge(request.getAverageAge())
+                .averageGender(request.getAverageGender())
+                .build();
+
         teamRepository.save(dto);
+
+/*        TeamMemberDTO memberDTO = TeamMemberDTO
+                .builder()
+                .userId(userid)
+                .teamId(dto.getId())
+                .privilege(1) // 0 : 관리자, 1 : 팀장, 2 : 팀원
+                .build();
+
+        teamMemberRepository.save(memberDTO);*/
     }
 
     // 팀 리스트 처리
@@ -49,50 +65,35 @@ public class TeamService {
     // 팀 정보 수정
     public TeamDTO teamUpdate(long id, TeamCreateRequest request) {
         TeamDTO existingTeam = teamRepository
-            .findById(id)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Team not found"));
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Team not found"));
 
         TeamDTO updatedTeam = TeamDTO
-            .builder()
-            .id(existingTeam.getId())
-            .sportsId(request.getSportsId())
-            .teamName(request.getTeamName())
-            .uri(request.getUri())
-            .useWeek(request.getUseWeek())
-            .useTime(request.getUseTime())
-            .averageAge(request.getAverageAge())
-            .averageGender(request.getAverageGender())
-            .build();
+                .builder()
+                .id(existingTeam.getId())
+                .sportsId(request.getSportsId())
+                .teamName(request.getTeamName())
+                .uri(request.getUri())
+                .useWeek(request.getUseWeek())
+                .useTime(request.getUseTime())
+                .averageAge(request.getAverageAge())
+                .averageGender(request.getAverageGender())
+                .build();
 
         return teamRepository.save(updatedTeam);
     }
 
     // 팀 삭제
+    @Transactional
     public void teamDelete(long id) {
+        if(recruitRepository.findByTeamId(id) != null) {
+            recruitRepository.deleteRecruitsByTeamId(id);
+        }
         teamRepository.deleteById(id);
     }
 
-    // 데이터 저장 비트 연산
-    public long saveValue(List<Long> selectedColumns) {
-        int selectedValues = 0;
-        for(Long id : selectedColumns) {
-            selectedValues |= 1 << id.intValue();
-        }
-        return selectedValues;
-    }
-
-    // 비트 데이터 반환
-    public List<Long> extractValue(long selectedValues) {
-        List<Long> extractedValues = new ArrayList<>();
-        int index = 0;
-
-        while (selectedValues > 0) {
-            if ((selectedValues & 1) == 1) {
-                extractedValues.add((long) index);
-            }
-            selectedValues >>= 1;
-            index++;
-        }
-        return extractedValues;
+    // 모집글이 있는 팀 정보
+    public Page<TeamDTO> teamViewWithRecruit(Pageable pageable){
+        return teamRepository.findAllWithRecruit(pageable);
     }
 }
