53a5178976bd9eed68fc5ba7eb0436571370af3c
oqsis <leaunk@gmail.com>
2023-09-04 14:51:52 +0900

    09041451 수정중커밋

--- src/main/java/com/multicampus/matchcode/controller/hyuk/MatchMemberController.java
+++ src/main/java/com/multicampus/matchcode/controller/hyuk/MatchMemberController.java
@@ -29,24 +29,22 @@ public class MatchMemberController {
     private MatchMemberService matchMemberService;
 
     // 모집글 작성 매핑
-    @GetMapping("/post/test")
+    @GetMapping("/addmatchmember6")
     public String writeMatch(@PathVariable("matchid") Long matchId, Model model) {
         if (matchMemberService.isMatchExist(matchId)) {
             model.addAttribute("message", "참여중인 매치입니다.");
-            /*model.addAttribute("searchUrl", "/match/list"); // 임시 mapping 주소*/
+            model.addAttribute("searchUrl", "/match/list"); // 임시 mapping 주소
             return "hyem/message";
         } else {
-/*            MatchMemberDTO matchMemberDTO = new MatchMemberDTO();
-            model.addAttribute("recruit", matchMemberDTO);*/
+            MatchMemberDTO matchMemberDTO = new MatchMemberDTO();
+            model.addAttribute("recruit", matchMemberDTO);
             return "/match/list";
         }
     }
 
-    // 매치장 추가
-    @PostMapping("/addmatchmember")
-    public String addMatchMember(
-            Long id, Model model
-    ) throws Exception {
+    // 매치원 추가
+    @PostMapping("/addmatchmember3")
+    public String addMatchMember(@RequestParam("id") Long id, Model model) throws Exception {
         MatchMemberDTO matchMemberDTO = matchMemberService.matchMemberFind(id);
         long matchId = matchMemberDTO.getMatchId();
         long memberId = matchMemberDTO.getMemberId();
@@ -55,6 +53,28 @@ public class MatchMemberController {
         model.addAttribute("searchUrl", "/match/list");
         return "hyem/message";
     }
+
+/*    @GetMapping("/applymatch")
+    public String applyMatch(@PathVariable("no") Long matchId, Model model) {
+        if (matchMemberService.isMatchExist(matchId)) {
+            model.addAttribute("message", "참여중인 매치입니다.");
+            model.addAttribute("searchUrl", "/match/list");
+            return "hyem/message";
+        } else {
+            // 팀 멤버 추가 로직을 여기에 직접 구현
+            MatchMemberDTO matchMemberDTO = new MatchMemberDTO();
+            // matchMemberDTO 설정
+            long memberId = matchMemberDTO.getMemberId();
+            matchMemberService.addMatchMember(matchId, memberId);
+
+            // 추가 후 메시지 추가
+            model.addAttribute("message", "매치신청에 완료했습니다.");
+            model.addAttribute("searchUrl", "/match/list");
+
+            return "hyem/message";
+        }
+    }*/
+
 }
 
 
